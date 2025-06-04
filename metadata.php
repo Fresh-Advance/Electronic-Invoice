9e7f4664f4c4f7be9c7e18b073d72d7058319c5d
Anton Fedurtsya <anton@fedurtsya.com>
2025-06-04 19:37:16 +0300

    Initial commit

---
+++ metadata.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information
+ */
+$aModule = [
+    'id'          => 'oe_moduletemplate',
+    'title'       => 'OxidEsales Module Template (OEMT)',
+    'description' => 'Module Template – Clean Skeleton for OXID Modules',
+    'thumbnail'   => 'pictures/logo.png',
+    'version'     => '4.0.0',
+    'author'      => 'OXID eSales AG',
+    'url'         => '',
+    'email'       => '',
+    'extend'      => [],
+];
